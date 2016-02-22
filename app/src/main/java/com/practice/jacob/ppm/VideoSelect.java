d9d5d6c088fc6b3e37ff0c59d5e5ba90971b6695
jake61392 <jake61392@gmail.com>
2016-02-22 08:32:19 -0700

    added audio button code

--- app/src/main/java/com/practice/jacob/ppm/VideoSelect.java
+++ app/src/main/java/com/practice/jacob/ppm/VideoSelect.java
@@ -1,13 +1,33 @@
 package com.practice.jacob.ppm;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class VideoSelect extends AppCompatActivity {
+    private MediaPlayer audio;
+    private boolean isStopped = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_select);
+        audio = MediaPlayer.create(this, R.raw.uchtdorf);
+    }
+
+    public void playSound(View v) {
+
+        if (!audio.isPlaying()) {
+            if (isStopped){
+                audio = MediaPlayer.create(this, R.raw.uchtdorf);
+                isStopped = false;
+            }
+            audio.start();
+        }
+        else {
+            audio.stop();
+            isStopped = true;
+        }
     }
 }
