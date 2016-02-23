db03ac65b9f76b2e8a0ee431722199fb5bd77c6d
Jacob <jkonshuk32@gmail.com>
2016-02-23 09:16:11 -0700

    did some stuff

--- app/src/main/java/com/practice/jacob/ppm/VideoSelect.java
+++ app/src/main/java/com/practice/jacob/ppm/VideoSelect.java
@@ -3,24 +3,29 @@ package com.practice.jacob.ppm;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceView;
 import android.view.View;
 
+import java.io.IOException;
+
 public class VideoSelect extends AppCompatActivity {
+
     private MediaPlayer audio;
     private boolean isStopped = false;
+    private SurfaceView mySurface;
+    private MediaPlayer myPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_select);
-        audio = MediaPlayer.create(this, R.raw.uchtdorf);
+        audio = MediaPlayer.create(this, R.raw.theholyghostasyourcompanion);
     }
 
     public void playSound(View v) {
-
         if (!audio.isPlaying()) {
             if (isStopped){
-                audio = MediaPlayer.create(this, R.raw.uchtdorf);
+                audio = MediaPlayer.create(this, R.raw.theholyghostasyourcompanion);
                 isStopped = false;
             }
             audio.start();
@@ -29,5 +34,21 @@ public class VideoSelect extends AppCompatActivity {
             audio.stop();
             isStopped = true;
         }
+    }
+    
+    public void playMovie (View v) {
+        mySurface = (SurfaceView)(findViewById(R.id.videoView));
+        myPlayer = MediaPlayer.create(this, R.raw.patternsoflight);
+
+        myPlayer.setDisplay(mySurface.getHolder());
+        //try {
+            //myPlayer.setDataSource(this.getResources().getResourceName(R.raw.patternsoflight));
+            //myPlayer.prepare();
+       // } //catch (IOException e) {
+            //e.printStackTrace();
+        //}
+        myPlayer.start();
+
+        //myPlayer.release();
     }
 }
