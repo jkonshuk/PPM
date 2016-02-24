7600ac11ba9fc2e7e7c2ffc031b375b1d57defc3
jake61392 <jake61392@gmail.com>
2016-02-24 08:48:00 -0700

    added audio file test

--- app/src/test/java/com/practice/jacob/ppm/ExampleUnitTest.java
+++ app/src/test/java/com/practice/jacob/ppm/ExampleUnitTest.java
@@ -2,19 +2,33 @@ package com.practice.jacob.ppm;
 
 import org.junit.Test;
 
+import java.io.File;
+
 import static org.junit.Assert.*;
 
 /**
  * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() throws Exception {
-        assertEquals(4, 2 + 2);
-    }
 
     @Test
     public void videoPlaying () throws Exception {
 
+    }
+
+    /*
+     * Jakob Mueller's test code
+     */
+    @Test
+    public void audioFile() throws Exception {
+        assert(new File("C:\\Users\\Jakob\\AndroidStudioProjects\\PPM\\app\\src\\main\\res\\raw\\uchtdorf.mp3").isFile());
+    }
+
+    /*
+     * Jakob Mueller
+     */
+    @Test
+    public void audioRunning() throws Exception {
+        //assert()
     }
 }
