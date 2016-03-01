98fc6a142ca385f13b9134c65005433e21473014
Jacob <jkonshuk32@gmail.com>
2016-03-01 09:17:49 -0700

    Changed a bunch of stuff with the videos

--- app/src/main/java/com/practice/jacob/ppm/VideoSelect.java
+++ app/src/main/java/com/practice/jacob/ppm/VideoSelect.java
@@ -16,7 +16,6 @@ public class VideoSelect extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_select);
         surface = (SurfaceView) (findViewById(R.id.videoView));
-        video = new VideoPlayer(this, surface, R.raw.patternsoflight);
         audio = new AudioPlayer(this, R.raw.uchtdorf);
     }
 
@@ -26,7 +25,7 @@ public class VideoSelect extends AppCompatActivity {
         audio.play();
     }
 
-    public void playVideo (View v){
+    public void playVideo (){
         if (!audio.stopped())
             audio.pause();
         video.play();
@@ -34,26 +33,53 @@ public class VideoSelect extends AppCompatActivity {
 
     public void changeButton (View v){
         Button button = (Button)(findViewById(v.getId()));
-        if(button.getText().equals(""))
+        if(button.getText().equals("Audio1"))
             button.setText("something else");
-        if(button.getText().equals("something"))
+        if(button.getText().equals("Audio2"))
             button.setText("something else");
-        if(button.getText().equals("something"))
+        if(button.getText().equals("Audio3"))
             button.setText("something else");
-        if(button.getText().equals("something"))
+        if(button.getText().equals("Audio4"))
             button.setText("something else");
-        if(button.getText().equals("something"))
+        if(button.getText().equals("Audio5"))
             button.setText("something else");
-        if(button.getText().equals("something"))
+        if(button.getText().equals("Audio6"))
             button.setText("something else");
-        if(button.getText().equals("something"))
+        if(button.getText().equals("Audio7"))
             button.setText("something else");
-        if(button.getText().equals("something"))
+        if(button.getText().equals("Audio8"))
             button.setText("something else");
-        if(button.getText().equals("something"))
+        if(button.getText().equals("Audio9"))
             button.setText("something else");
-        if(button.getText().equals("something"))
-            button.setText("something else");
+        if(button.getText().equals("Video1")) {
+            video = new VideoPlayer(this, surface, R.raw.patterns_of_light);
+            playVideo();
+        }
+        if(button.getText().equals("Video2")) {
+            video = new VideoPlayer(this, surface, R.raw.a_book_of_mormon_story);
+            playVideo();
+        }
+        if(button.getText().equals("Video3")) {
+            video = new VideoPlayer(this, surface, R.raw.flecks_of_gold);
+            playVideo();
+        }
+        if(button.getText().equals("Video4")) {
+            video = new VideoPlayer(this, surface, R.raw.good_things_to_come);
+            playVideo();
+        }
+        if(button.getText().equals("Video5")) {
+            video = new VideoPlayer(this, surface, R.raw.let_us_be_men);
+            playVideo();
+        }
+        if(button.getText().equals("Video6"))
+            video = new VideoPlayer(this, surface, R.raw.patternsoflight);
+        if(button.getText().equals("Video7"))
+            video = new VideoPlayer(this, surface, R.raw.patternsoflight);
+        if(button.getText().equals("Video8"))
+            video = new VideoPlayer(this, surface, R.raw.patternsoflight);
+        if(button.getText().equals("Video9"))
+            video = new VideoPlayer(this, surface, R.raw.patternsoflight);
+
     }
 
 }
