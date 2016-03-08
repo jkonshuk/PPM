cb42cbd2441255245c6aee066701deb78083517d
Jacob <jkonshuk32@gmail.com>
2016-03-08 11:35:02 -0700

    Won't set the display in play for the videoplayer no idea why. This one seems totally beyond me.

--- app/src/main/java/com/practice/jacob/ppm/VideoPlayer.java
+++ app/src/main/java/com/practice/jacob/ppm/VideoPlayer.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
 import java.io.IOException;
@@ -12,23 +14,35 @@ import java.io.IOException;
  * Created by taser_000 on 2/27/2016.
  */
 public class VideoPlayer extends AppCompatActivity {
-    private MediaPlayer myPlayer;
     private SurfaceView mySurface;
+    private MediaPlayer myPlayer;
+    private SurfaceHolder holder;
     private boolean isStopped = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_video_player);
-        Intent intent = getIntent();
         mySurface = (SurfaceView)(findViewById(R.id.surfaceView));
-        myPlayer = MediaPlayer.create(this, intent.getIntExtra("VIDEO", R.raw.a_book_of_mormon_story));
+        holder = mySurface.getHolder();
         play();
     }
 
     public void play() {
-        myPlayer.setDisplay(mySurface.getHolder());
-        myPlayer.setSurface(mySurface.getHolder().getSurface());
+        Intent intent = getIntent();
+        myPlayer = MediaPlayer.create(this, intent.getIntExtra("VIDEO", R.raw.a_book_of_mormon_story));
+
+        try {
+            //myPlayer.setDisplay(mySurface.getHolder());
+            myPlayer.setSurface(mySurface.getHolder().getSurface());
+        }
+        catch (Exception e){
+            Log.w("HolderError", mySurface.getHolder().toString());
+            Log.w("HolderError", mySurface.getHolder().getSurface().toString());
+            Log.e("HolderError", e.getMessage());
+            Log.e("HolderError", e.toString());
+            Log.e("HolderError", e.getLocalizedMessage());
+        }
 
         if (!myPlayer.isPlaying()) {
             if (isStopped) {
